3a90dd01399f0473b3bbf23e8c510842277007ca
pavelcodixis <pangelov@codixis.com>
2023-10-04 12:23:41 +0300

    Add fixtures and dependencies

--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -2,7 +2,6 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ImageRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Ramsey\Uuid\Doctrine\UuidGenerator;
@@ -10,7 +9,7 @@ use Ramsey\Uuid\UuidInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: ImageRepository::class)]
-#[ApiResource]
+// #[ApiResource]
 class Image
 {
     #[ORM\Id]
@@ -22,15 +21,28 @@ class Image
 
     #[ORM\Column(length: 255)]
     #[Groups(["alcohol"])]
+    private ?string $filename = null;
+
+    #[Groups(["alcohol"])]
     private ?string $name = null;
 
-    #[ORM\Column(length: 255)]
     #[Groups(["alcohol"])]
     private ?string $url = null;
 
     public function getId(): ?UuidInterface
     {
         return $this->id;
+    }
+
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+
+    public function setFilename(string $filename): static
+    {
+        $this->filename = $filename;
+        return $this;
     }
 
     public function getName(): ?string
@@ -41,19 +53,21 @@ class Image
     public function setName(string $name): static
     {
         $this->name = $name;
-
         return $this;
     }
 
     public function getUrl(): ?string
     {
-        return $this->url;
+        if ($this->name) {
+            return '/storage/images/' . $this->filename;
+        }
+
+        return null;
     }
 
     public function setUrl(string $url): static
     {
         $this->url = $url;
-
         return $this;
     }
 }
